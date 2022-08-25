c88b6813894845981914defb1a59c97d539d4983
Pablo Viana <96837989+pablovianas@users.noreply.github.com>
2022-08-25 19:02:08 -0300

    updated script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,17 @@
-const getNewAdviceButton = document.querySelector('.advice-update');
-const adviceNumber = document.querySelector('.advice-id')
-const adviceDescription= document.querySelector('.advice-description')
-
+const adviceUpdateButton = document.querySelector(".advice-update");
+const adviceNumber = document.querySelector(".advice-id");
+const adviceDescription = document.querySelector(".advice-description");
 
 async function getAdvice() {
-    const response = await fetch('https://api.adviceslip.com/advice');
-    const adviceContent = await response.json()
-    const adviceId = `Advice #${adviceContent.slip.id}`
-    const adviceText = `"${adviceContent.slip.advice}"`
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const adviceContent = await response.json();
+    const adviceId = `Advice #${adviceContent.slip.id}`;
+    const adviceText = `"${adviceContent.slip.advice}"`;
 
-    adviceNumber.innerHTML = adviceId
-    adviceDescription.innerHTML = adviceText
+    adviceNumber.innerHTML = adviceId;
+    adviceDescription.innerHTML = adviceText;
 }
 
-getNewAdviceButton.addEventListener('click', getAdvice())
-window.onload = async () => {
-    getAdvice()
-}
+adviceUpdateButton.addEventListener("click", getAdvice);
+
+getAdvice();
